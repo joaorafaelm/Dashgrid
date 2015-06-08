d19b8521029bc4e626ef614b67bd18f9d4cab828
João Rafael <joaoraf@me.com>
2015-06-07 21:06:54 -0300

    added grunt concat for javascript

--- gruntfile.js
+++ gruntfile.js
@@ -6,24 +6,31 @@ module.exports = function(grunt) {
                     paths: ["src/less"],
                     cleancss: true // minification
                 },
-                files: {"build/css/dashgrid.css": "src/less/main.less"}
+                files: {"build/css/dashgrid.min.css": "src/less/main.less"}
             }
         },
         watch: {
             css: {
-                files: ['src/less/*.less'],
-                tasks: ['less']
+                files: ['src/less/*.less', 'src/js/*.js'],
+                tasks: ['less', 'concat']
             }
         },
         jshint: {
             dashgrid: ['src/js/*.js']
-        }
+        },
+        concat: {
+            build: {
+                src: ['src/js/*.js'],
+                dest: 'build/js/dashgrid.js'
+            }
+        }   
     });
     
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     
-    grunt.registerTask('default', ['jshint', 'less']);
+    grunt.registerTask('default', ['jshint', 'less', 'concat']);
 
 };
